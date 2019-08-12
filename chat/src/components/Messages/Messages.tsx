e0fb69adb992ffaddb2f99a064f9fe41e86a48e4
Yury Chuchvaha <gagael23@live.com>
2019-08-12 10:56:26 +0300

    feat: add Messages component with styles

---
+++ chat/src/components/Messages/Messages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Messages.scss';
+
+const Messages: React.FC = () => {
+  return (
+    <div className="messages">
+      <p>messages</p>
+    </div>
+  );
+};
+
+export default Messages;
