5d4a93f7b658f16926927a9f2d87bc23a03ff8d8
Yury Chuchvaha <gagael23@live.com>
2019-08-12 11:08:43 +0300

    feat: update App component and change styles

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Input from './components/Input/Input';
+import Status from './components/Status/Status';
+import Messages from './components/Messages/Messages';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="main-wrapper">
-      <input />
-    </div>
+    <section className="chat-wrapper">
+      <Status />
+      <Messages />
+      <Input />
+    </section>
   );
 };
 
