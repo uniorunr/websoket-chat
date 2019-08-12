65b14e6fc093ed5e2eb53c264ae0b09a9ba81545
Yury Chuchvaha <gagael23@live.com>
2019-08-12 10:57:39 +0300

    feat: add Status component with styles

---
+++ chat/src/components/Status/Status.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Status.scss';
+
+const Status: React.FC = () => {
+  return (
+    <div className="status">
+      <p>Status:</p>
+    </div>
+  );
+};
+
+export default Status;
