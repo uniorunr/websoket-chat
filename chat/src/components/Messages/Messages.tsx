413a89d58204cb75c78f91066bd05c4410ff8358
Yury Chuchvaha <gagael23@live.com>
2019-08-13 14:34:24 +0300

    feat: update styles of status and Messages components; add scroll to bottom functionality

--- chat/src/components/Messages/Messages.tsx
+++ chat/src/components/Messages/Messages.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { StoreState } from '../../types';
+import { connect } from 'react-redux';
 import './Messages.scss';
 
-const Messages: React.FC = () => {
-  return (
-    <div className="messages">
-      <div className="messages__content">
-        <p>messages</p>
-      </div>
-    </div>
-  );
-};
+class Messages extends Component {
+  private messagesEnd: React.RefObject<HTMLDivElement>;
 
-export default Messages;
+  constructor(props: any) {
+    super(props);
+    this.messagesEnd = React.createRef();
+  }
+
+  scrollToBottom = () => {
+    const anchor = this.messagesEnd.current;
+    if (anchor) anchor.scrollIntoView();
+  };
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render() {
+    const { messages }: any = this.props;
+
+    return (
+      <div className="messages">
+        <div className="messages__content">
+          {messages.map((message: MessageObject) => (
+            <div key={message.id}>
+              <p className="messages__author"> {message.from}</p>
+              <p className="messages__message">{message.message}</p>
+            </div>
+          ))}
+          <div
+            style={{ float: 'left', clear: 'both' }}
+            ref={this.messagesEnd}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+interface MessageObject {
+  id: string;
+  message: string;
+  from: string;
+}
+
+const mapStateToProps = (state: StoreState) => ({
+  messages: state.messages
+});
+
+export default connect(mapStateToProps)(Messages);
