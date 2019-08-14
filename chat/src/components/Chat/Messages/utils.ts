961fdc3a7f8fa84fc448ef43ce86cd523d2a9a5c
Yury Chuchvaha <gagael23@live.com>
2019-08-14 09:11:27 +0300

    fix: fix index of month in message date

--- chat/src/components/Chat/Messages/utils.ts
+++ chat/src/components/Chat/Messages/utils.ts
@@ -6,7 +6,8 @@ const formatDate = (ms: number) => {
   const seconds =
     date.getSeconds() >= 10 ? date.getSeconds() : `0${date.getSeconds()}`;
   const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`;
-  const month = date.getMonth() >= 10 ? date.getMonth() : `0${date.getMonth()}`;
+  const month =
+    date.getMonth() >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
   const year = date.getFullYear();
 
   return `${hours}:${minutes}:${seconds} ${day}-${month}-${year}`;
